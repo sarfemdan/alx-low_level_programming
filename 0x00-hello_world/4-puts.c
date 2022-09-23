a63d9b6ac8c0c46813dc36fe5a164cdab0595bcd
ADEDOYIN OLUWAFEMI DANIEL <sarfemdan@gmail.com>
2022-09-23 01:51:32 -0700

    femi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Prints the phrase "programming is like building a multilingual puzzle
- * Return: 0 if exited proprely, non-zeor otherwise 
- * /
- int main(void)
- {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
- }
+* main - Printing
+*
+* Return: Always 0 (Successful)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual
+puzzle");
+return (0);
+}
